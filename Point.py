4835bfe93feacbde5e1b1bd2a9385636b9904fb2
Aliaksei Poliak <aliakseipv@gmail.com>
2022-09-02 01:46:05 -0400

    Adding program Distance

--- Point.py
+++ Point.py
@@ -5,10 +5,10 @@ n = int(input())
 if n == 1:
     print("x > 0 and y > 0")
 elif n == 2:
-    print("x > 0 and y < 0")
+    print("x < 0 and y > 0")
 elif n == 3:
     print("x < 0 and y < 0")
 elif n == 4:
-    print("x < 0 and y > 0")
+    print("x > 0 and y < 0")
 else:
     print("Default")
