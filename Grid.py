4c819957f8c1ff9f47c572128648556b7754c7cb
Aliaksei Poliak <aliakseipv@gmail.com>
2022-09-02 01:12:01 -0400

    Adding program Grid

---
+++ Grid.py
@@ -0,0 +1,20 @@
+
+print("Enter x: ")
+x = int(input())
+
+print("Enter y: ")
+y = int(input())
+
+if x != 0 or y != 0:
+    if x > 0 and y > 0:
+        print("Quarter: 1")
+    elif x > 0 and y < 0:
+        print("Quarter: 2")
+    elif x < 0 and y < 0:
+        print("Quarter: 3")
+    elif x < 0 and y > 0:
+        print("Quarter: 4")
+else:
+    print("x and y can not be equal to zero")
+
+
