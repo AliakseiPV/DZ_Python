e90e817506a06c7c359e85665d265832c0c43ece
Aliaksei Poliak <aliakseipv@gmail.com>
2022-09-14 08:15:00 -0400

    Edits

--- DZ4/NonRepeatingList.py
+++ DZ4/NonRepeatingList.py
@@ -2,24 +2,28 @@
 #неповторяющихся элементов исходной последовательности.
 #[1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
 
+
 inputList = [1, 1, 2, 3, 4, 5, 5]
 outputList = list(set(inputList))
 finalList = []
 
-
-count = 0
-for i in outputList:
-
-    for j in inputList:
-        if (i == j):
-            count += 1
-        print(f"i:{i} ////// j:{j} ///////// count:{count}")
-
-    if(count <= 1):
-        finalList.append(i)
+def NonRepeatingList(list1, list2, list3):
     count = 0
+    for i in list2:
 
-print(finalList)
+        for j in list1:
+            if (i == j):
+                count += 1
+
+        if(count <= 1):
+            list3.append(i)
+        count = 0
+    return list3   
+
+
+print(f"{inputList}  =>  {NonRepeatingList(inputList,outputList,finalList)}")
+
+
 
 
 
