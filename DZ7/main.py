73e02fb48f4883638539f025d3406ade437b81bf
Aliaksei Poliak <aliakseipv@gmail.com>
2022-09-29 05:45:00 -0400

    Adding weather

--- DZ7/main.py
+++ DZ7/main.py
@@ -6,11 +6,12 @@ import html_creator
 import txt_creator
 import xml_creator
 
-#data = data_provider.data_collection()
+data = data_provider.data_collection()
 
-#html_creator.create(data)
-#xml_creator.create(data)
-#txt_creator.create(data)
+html_creator.create(data)
+xml_creator.create(data)
+txt_creator.create(data)
 
 #print(user_interface.find_number())
+
 print(user_interface.find_full_name())
